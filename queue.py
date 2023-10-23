790dbb2d582e3d4de00695c5364d6dcf3af5e6b3
thomascourtney <64978864+thomascourtney@users.noreply.github.com>
2023-10-23 14:21:34 -0400

    changes

--- queue.py
+++ queue.py
@@ -24,19 +24,12 @@ class QueueSimulation:
         for customer_id in range(1, self.num_customers + 1):
             customer = Customer(customer_id, self.arrival_mean, self.arrival_stddev, self.service_mean, self.service_stddev)
 
-            # Calculate waiting time (time spent in the queue)
             customer.waiting_time = max(0, self.simulation_time - customer.inter_arrival_time)
 
-            # Calculate total system time
-            customer.total_system_time = customer.inter_arrival_time + customer.waiting_time + customer.service_time
-
-            # Update simulation time
             self.simulation_time += customer.inter_arrival_time + customer.service_time
-
-            # Append customer information
+            
             self.customers.append(customer)
-
-            # Record waiting and service times for averaging
+            
             self.waiting_times.append(customer.waiting_time)
             self.service_times.append(customer.service_time)
 
@@ -46,15 +39,9 @@ class QueueSimulation:
             print(f"Inter-arrival time: {customer.inter_arrival_time:.2f} sec")
             print(f"Waiting time: {customer.waiting_time:.2f} sec")
             print(f"Service time: {customer.service_time:.2f} sec")
-            print(f"Total system time: {customer.total_system_time:.2f} sec")
             print()
 
-    def calculate_and_display_averages(self):
-        avg_waiting_time = sum(self.waiting_times) / self.num_customers
-        avg_service_time = sum(self.service_times) / self.num_customers
 
-        print(f"Average Waiting Time for All Customers: {avg_waiting_time:.2f} sec")
-        print(f"Average Service Time for All Customers: {avg_service_time:.2f} sec")
 
 if __name__ == "__main__":
     num_customers = 15
@@ -66,4 +53,3 @@ if __name__ == "__main__":
     simulation = QueueSimulation(num_customers, arrival_mean, arrival_stddev, service_mean, service_stddev)
     simulation.run_simulation()
     simulation.display_customer_information()
-    simulation.calculate_and_display_averages()
